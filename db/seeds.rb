980f260f444064b0d0216e2a4d9b68819ea570c5
524855 <annguyen@new-host-7.home>
2017-05-19 14:47:55 -0400

    add scores data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,121 +7,1397 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-Blackcard.create([
-  {
-    question: "Why can't I sleep at night?",
-    pick: 1
-  },
-  {
-    question: "I got 99 problems but _ ain't one.",
-    pick: 1
-  },
-  {
-    question: "What's a girl's best friend?",
-    pick: 1
-  },
-  {
-    question: "What's that smell?",
-    pick: 1
-  },
-  {
-    question: "This is the way the world ends / This is the way the world ends / Not with a bang but with _.",
-    pick: 1
-  },
-  {
-    question: "What is Batman's guilty pleasure?",
-    pick: 1
-  },
-  {
-    question: "TSA guidelines now prohibit _ on airplanes.",
-    pick: 1
-  },
-  {
-    question: "What ended my last relationship?",
-    pick: 1
-  },
-  {
-    question: "MTV's new reality show features eight washed-up celebrities living with _.",
-    pick: 1
-  },
-  {
-    question: "I drink to forget _.",
-    pick: 1
-  },
-  {
-    question: "What's that sound?",
-    pick: 1
-  },
-  {
-    question: "What's the next Happy Meal&reg; toy?",
-    pick: 1
-  },
-  {
-    question: "It's a pity that kids these days are all getting involved with _.",
-    pick: 1
-  },
-  {
-    question: "In the new Disney Channel Original Movie, Hannah Montana struggles with _ for the first time.",
-    pick: 1
-  },
-  {
-    question: "_. That's how I want to die.",
-    pick: 1
-  }
-  ])
-
-
-Whitecard.create([
-  {
-     answer: "Coat hanger abortions."
-   },
-  {
-    answer:"Man meat."
-  },
-  {
-    answer:"Autocannibalism."
-  },
-  {
-     answer:"Vigorous jazz hands."
-  },
-
-  {
-     answer:"Flightless birds."
-  },
-  {
-     answer:"Pictures of boobs."
-  },
-  {
-     answer:"Doing the right thing."
-  },
-  {
-     answer:"The violation of our most basic human rights."
-  },
-  {
-     answer:"Viagra&reg;."
-  },
-  {
-     answer:"Self-loathing."
-  },
-  {
-     answer:  "Spectacular abs."
-  },
-  {
-     answer:  "A balanced breakfast."
-  },
-  {
-     answer:  "Roofies."
-  },
-  {
-     answer:   "Concealing a boner."
-  },
-  {
-     answer:   "Amputees."
-  },
-  {
-     answer:    "The Big Bang."
-  },
-  {
-     answer:   "Former President George W. Bush."
-  }
-])
+# Blackcard.create([
+#   {
+#     question: "Why can't I sleep at night?",
+#     pick: 1
+#   },
+#   {
+#     question: "I got 99 problems but _ ain't one.",
+#     pick: 1
+#   },
+#   {
+#     question: "What's a girl's best friend?",
+#     pick: 1
+#   },
+#   {
+#     question: "What's that smell?",
+#     pick: 1
+#   },
+#   {
+#     question: "This is the way the world ends / This is the way the world ends / Not with a bang but with _.",
+#     pick: 1
+#   },
+#   {
+#     question: "What is Batman's guilty pleasure?",
+#     pick: 1
+#   },
+#   {
+#     question: "TSA guidelines now prohibit _ on airplanes.",
+#     pick: 1
+#   },
+#   {
+#     question: "What ended my last relationship?",
+#     pick: 1
+#   },
+#   {
+#     question: "MTV's new reality show features eight washed-up celebrities living with _.",
+#     pick: 1
+#   },
+#   {
+#     question: "I drink to forget _.",
+#     pick: 1
+#   },
+#   {
+#     question: "What's that sound?",
+#     pick: 1
+#   },
+#   {
+#     question: "What's the next Happy Meal&reg; toy?",
+#     pick: 1
+#   },
+#   {
+#     question: "It's a pity that kids these days are all getting involved with _.",
+#     pick: 1
+#   },
+#   {
+#     question: "In the new Disney Channel Original Movie, Hannah Montana struggles with _ for the first time.",
+#     pick: 1
+#   },
+#   {
+#     question: "_. That's how I want to die.",
+#     pick: 1
+#   }
+#   ])
+#
+# Whitecard.create([
+#   {
+#     answer: "Coat hanger abortions."
+#    },
+#   {
+#     answer:"Man meat."
+#   },
+#   {
+#       answer:"Autocannibalism."
+#   },
+#   {
+#      answer:"Vigorous jazz hands."
+#   },
+#
+#   {
+#      answer:"Flightless birds."
+#   },
+#   {
+#      answer:"Pictures of boobs."
+#   },
+#   {
+#      answer:"Doing the right thing."
+#   },
+#   {
+#      answer:"The violation of our most basic human rights."
+#   },
+#   {
+#      answer:"Viagra&reg;."
+#   },
+#   {
+#      answer:"Self-loathing."
+#   },
+#   {
+#      answer:  "Spectacular abs."
+#   },
+#   {
+#      answer:  "A balanced breakfast."
+#   },
+#   {
+#      answer:  "Roofies."
+#   },
+#   {
+#      answer:   "Concealing a boner."
+#   },
+#   {
+#      answer:   "Amputees."
+#   },
+#   {
+#      answer:    "The Big Bang."
+#   },
+#   {
+#      answer:   "Former President George W. Bush."
+#   }
+# ])
+Score.create([
+{
+ blackcard_id: 1,
+whitecard_id: 1,
+score: 7
+ },
+ {
+ blackcard_id: 1,
+whitecard_id: 2,
+score: 4
+ },
+ {
+ blackcard_id: 1,
+whitecard_id: 3,
+score: 2
+ },
+ {
+ blackcard_id: 1,
+whitecard_id: 4,
+score: 0
+ },
+ {
+ blackcard_id: 1,
+whitecard_id: 5,
+score: 9
+ },
+ {
+ blackcard_id: 1,
+whitecard_id: 6,
+score: 5
+ },
+ {
+ blackcard_id: 1,
+whitecard_id: 7,
+score: 0
+ },
+ {
+ blackcard_id: 1,
+whitecard_id: 8,
+score: 6
+ },
+ {
+ blackcard_id: 1,
+whitecard_id: 9,
+score: 8
+ },
+ {
+ blackcard_id: 1,
+whitecard_id: 10,
+score: 8
+ },
+ {
+ blackcard_id: 1,
+whitecard_id: 11,
+score: 5
+ },
+ {
+ blackcard_id: 1,
+whitecard_id: 12,
+score: 6
+ },
+ {
+ blackcard_id: 1,
+whitecard_id: 13,
+score: 5
+ },
+ {
+ blackcard_id: 1,
+whitecard_id: 14,
+score: 8
+ },
+ {
+ blackcard_id: 1,
+whitecard_id: 15,
+score: 3
+ },
+ {
+ blackcard_id: 1,
+whitecard_id: 16,
+score: 5
+ },
+ {
+ blackcard_id: 1,
+whitecard_id: 17,
+score: 4
+ },
+ {
+ blackcard_id: 2,
+whitecard_id: 1,
+score: 2
+ },
+ {
+ blackcard_id: 2,
+whitecard_id: 2,
+score: 9
+ },
+ {
+ blackcard_id: 2,
+whitecard_id: 3,
+score: 1
+ },
+ {
+ blackcard_id: 2,
+whitecard_id: 4,
+score: 1
+ },
+ {
+ blackcard_id: 2,
+whitecard_id: 5,
+score: 0
+ },
+ {
+ blackcard_id: 2,
+whitecard_id: 6,
+score: 6
+ },
+ {
+ blackcard_id: 2,
+whitecard_id: 7,
+score: 7
+ },
+ {
+ blackcard_id: 2,
+whitecard_id: 8,
+score: 0
+ },
+ {
+ blackcard_id: 2,
+whitecard_id: 9,
+score: 5
+ },
+ {
+ blackcard_id: 2,
+whitecard_id: 10,
+score: 7
+ },
+ {
+ blackcard_id: 2,
+whitecard_id: 11,
+score: 2
+ },
+ {
+ blackcard_id: 2,
+whitecard_id: 12,
+score: 6
+ },
+ {
+ blackcard_id: 2,
+whitecard_id: 13,
+score: 5
+ },
+ {
+ blackcard_id: 2,
+whitecard_id: 14,
+score: 7
+ },
+ {
+ blackcard_id: 2,
+whitecard_id: 15,
+score: 7
+ },
+ {
+ blackcard_id: 2,
+whitecard_id: 16,
+score: 2
+ },
+ {
+ blackcard_id: 2,
+whitecard_id: 17,
+score: 0
+ },
+ {
+ blackcard_id: 3,
+whitecard_id: 1,
+score: 6
+ },
+ {
+ blackcard_id: 3,
+whitecard_id: 2,
+score: 8
+ },
+ {
+ blackcard_id: 3,
+whitecard_id: 3,
+score: 0
+ },
+ {
+ blackcard_id: 3,
+whitecard_id: 4,
+score: 0
+ },
+ {
+ blackcard_id: 3,
+whitecard_id: 5,
+score: 9
+ },
+ {
+ blackcard_id: 3,
+whitecard_id: 6,
+score: 2
+ },
+ {
+ blackcard_id: 3,
+whitecard_id: 7,
+score: 9
+ },
+ {
+ blackcard_id: 3,
+whitecard_id: 8,
+score: 7
+ },
+ {
+ blackcard_id: 3,
+whitecard_id: 9,
+score: 2
+ },
+ {
+ blackcard_id: 3,
+whitecard_id: 10,
+score: 8
+ },
+ {
+ blackcard_id: 3,
+whitecard_id: 11,
+score: 3
+ },
+ {
+ blackcard_id: 3,
+whitecard_id: 12,
+score: 8
+ },
+ {
+ blackcard_id: 3,
+whitecard_id: 13,
+score: 9
+ },
+ {
+ blackcard_id: 3,
+whitecard_id: 14,
+score: 1
+ },
+ {
+ blackcard_id: 3,
+whitecard_id: 15,
+score: 4
+ },
+ {
+ blackcard_id: 3,
+whitecard_id: 16,
+score: 4
+ },
+ {
+ blackcard_id: 3,
+whitecard_id: 17,
+score: 9
+ },
+ {
+ blackcard_id: 4,
+whitecard_id: 1,
+score: 8
+ },
+ {
+ blackcard_id: 4,
+whitecard_id: 2,
+score: 0
+ },
+ {
+ blackcard_id: 4,
+whitecard_id: 3,
+score: 2
+ },
+ {
+ blackcard_id: 4,
+whitecard_id: 4,
+score: 3
+ },
+ {
+ blackcard_id: 4,
+whitecard_id: 5,
+score: 5
+ },
+ {
+ blackcard_id: 4,
+whitecard_id: 6,
+score: 0
+ },
+ {
+ blackcard_id: 4,
+whitecard_id: 7,
+score: 7
+ },
+ {
+ blackcard_id: 4,
+whitecard_id: 8,
+score: 5
+ },
+ {
+ blackcard_id: 4,
+whitecard_id: 9,
+score: 0
+ },
+ {
+ blackcard_id: 4,
+whitecard_id: 10,
+score: 9
+ },
+ {
+ blackcard_id: 4,
+whitecard_id: 11,
+score: 5
+ },
+ {
+ blackcard_id: 4,
+whitecard_id: 12,
+score: 1
+ },
+ {
+ blackcard_id: 4,
+whitecard_id: 13,
+score: 7
+ },
+ {
+ blackcard_id: 4,
+whitecard_id: 14,
+score: 9
+ },
+ {
+ blackcard_id: 4,
+whitecard_id: 15,
+score: 3
+ },
+ {
+ blackcard_id: 4,
+whitecard_id: 16,
+score: 5
+ },
+ {
+ blackcard_id: 4,
+whitecard_id: 17,
+score: 0
+ },
+ {
+ blackcard_id: 5,
+whitecard_id: 1,
+score: 4
+ },
+ {
+ blackcard_id: 5,
+whitecard_id: 2,
+score: 0
+ },
+ {
+ blackcard_id: 5,
+whitecard_id: 3,
+score: 2
+ },
+ {
+ blackcard_id: 5,
+whitecard_id: 4,
+score: 9
+ },
+ {
+ blackcard_id: 5,
+whitecard_id: 5,
+score: 6
+ },
+ {
+ blackcard_id: 5,
+whitecard_id: 6,
+score: 4
+ },
+ {
+ blackcard_id: 5,
+whitecard_id: 7,
+score: 5
+ },
+ {
+ blackcard_id: 5,
+whitecard_id: 8,
+score: 2
+ },
+ {
+ blackcard_id: 5,
+whitecard_id: 9,
+score: 1
+ },
+ {
+ blackcard_id: 5,
+whitecard_id: 10,
+score: 0
+ },
+ {
+ blackcard_id: 5,
+whitecard_id: 11,
+score: 4
+ },
+ {
+ blackcard_id: 5,
+whitecard_id: 12,
+score: 3
+ },
+ {
+ blackcard_id: 5,
+whitecard_id: 13,
+score: 6
+ },
+ {
+ blackcard_id: 5,
+whitecard_id: 14,
+score: 4
+ },
+ {
+ blackcard_id: 5,
+whitecard_id: 15,
+score: 2
+ },
+ {
+ blackcard_id: 5,
+whitecard_id: 16,
+score: 1
+ },
+ {
+ blackcard_id: 5,
+whitecard_id: 17,
+score: 4
+ },
+ {
+ blackcard_id: 6,
+whitecard_id: 1,
+score: 1
+ },
+ {
+ blackcard_id: 6,
+whitecard_id: 2,
+score: 5
+ },
+ {
+ blackcard_id: 6,
+whitecard_id: 3,
+score: 7
+ },
+ {
+ blackcard_id: 6,
+whitecard_id: 4,
+score: 5
+ },
+ {
+ blackcard_id: 6,
+whitecard_id: 5,
+score: 7
+ },
+ {
+ blackcard_id: 6,
+whitecard_id: 6,
+score: 9
+ },
+ {
+ blackcard_id: 6,
+whitecard_id: 7,
+score: 0
+ },
+ {
+ blackcard_id: 6,
+whitecard_id: 8,
+score: 7
+ },
+ {
+ blackcard_id: 6,
+whitecard_id: 9,
+score: 2
+ },
+ {
+ blackcard_id: 6,
+whitecard_id: 10,
+score: 2
+ },
+ {
+ blackcard_id: 6,
+whitecard_id: 11,
+score: 1
+ },
+ {
+ blackcard_id: 6,
+whitecard_id: 12,
+score: 7
+ },
+ {
+ blackcard_id: 6,
+whitecard_id: 13,
+score: 9
+ },
+ {
+ blackcard_id: 6,
+whitecard_id: 14,
+score: 0
+ },
+ {
+ blackcard_id: 6,
+whitecard_id: 15,
+score: 0
+ },
+ {
+ blackcard_id: 6,
+whitecard_id: 16,
+score: 5
+ },
+ {
+ blackcard_id: 6,
+whitecard_id: 17,
+score: 8
+ },
+ {
+ blackcard_id: 7,
+whitecard_id: 1,
+score: 6
+ },
+ {
+ blackcard_id: 7,
+whitecard_id: 2,
+score: 9
+ },
+ {
+ blackcard_id: 7,
+whitecard_id: 3,
+score: 9
+ },
+ {
+ blackcard_id: 7,
+whitecard_id: 4,
+score: 7
+ },
+ {
+ blackcard_id: 7,
+whitecard_id: 5,
+score: 4
+ },
+ {
+ blackcard_id: 7,
+whitecard_id: 6,
+score: 8
+ },
+ {
+ blackcard_id: 7,
+whitecard_id: 7,
+score: 6
+ },
+ {
+ blackcard_id: 7,
+whitecard_id: 8,
+score: 6
+ },
+ {
+ blackcard_id: 7,
+whitecard_id: 9,
+score: 5
+ },
+ {
+ blackcard_id: 7,
+whitecard_id: 10,
+score: 4
+ },
+ {
+ blackcard_id: 7,
+whitecard_id: 11,
+score: 8
+ },
+ {
+ blackcard_id: 7,
+whitecard_id: 12,
+score: 8
+ },
+ {
+ blackcard_id: 7,
+whitecard_id: 13,
+score: 0
+ },
+ {
+ blackcard_id: 7,
+whitecard_id: 14,
+score: 8
+ },
+ {
+ blackcard_id: 7,
+whitecard_id: 15,
+score: 5
+ },
+ {
+ blackcard_id: 7,
+whitecard_id: 16,
+score: 1
+ },
+ {
+ blackcard_id: 7,
+whitecard_id: 17,
+score: 4
+ },
+ {
+ blackcard_id: 8,
+whitecard_id: 1,
+score: 3
+ },
+ {
+ blackcard_id: 8,
+whitecard_id: 2,
+score: 4
+ },
+ {
+ blackcard_id: 8,
+whitecard_id: 3,
+score: 5
+ },
+ {
+ blackcard_id: 8,
+whitecard_id: 4,
+score: 7
+ },
+ {
+ blackcard_id: 8,
+whitecard_id: 5,
+score: 0
+ },
+ {
+ blackcard_id: 8,
+whitecard_id: 6,
+score: 5
+ },
+ {
+ blackcard_id: 8,
+whitecard_id: 7,
+score: 3
+ },
+ {
+ blackcard_id: 8,
+whitecard_id: 8,
+score: 2
+ },
+ {
+ blackcard_id: 8,
+whitecard_id: 9,
+score: 9
+ },
+ {
+ blackcard_id: 8,
+whitecard_id: 10,
+score: 0
+ },
+ {
+ blackcard_id: 8,
+whitecard_id: 11,
+score: 1
+ },
+ {
+ blackcard_id: 8,
+whitecard_id: 12,
+score: 8
+ },
+ {
+ blackcard_id: 8,
+whitecard_id: 13,
+score: 1
+ },
+ {
+ blackcard_id: 8,
+whitecard_id: 14,
+score: 5
+ },
+ {
+ blackcard_id: 8,
+whitecard_id: 15,
+score: 2
+ },
+ {
+ blackcard_id: 8,
+whitecard_id: 16,
+score: 0
+ },
+ {
+ blackcard_id: 8,
+whitecard_id: 17,
+score: 2
+ },
+ {
+ blackcard_id: 9,
+whitecard_id: 1,
+score: 6
+ },
+ {
+ blackcard_id: 9,
+whitecard_id: 2,
+score: 3
+ },
+ {
+ blackcard_id: 9,
+whitecard_id: 3,
+score: 1
+ },
+ {
+ blackcard_id: 9,
+whitecard_id: 4,
+score: 1
+ },
+ {
+ blackcard_id: 9,
+whitecard_id: 5,
+score: 4
+ },
+ {
+ blackcard_id: 9,
+whitecard_id: 6,
+score: 7
+ },
+ {
+ blackcard_id: 9,
+whitecard_id: 7,
+score: 6
+ },
+ {
+ blackcard_id: 9,
+whitecard_id: 8,
+score: 2
+ },
+ {
+ blackcard_id: 9,
+whitecard_id: 9,
+score: 2
+ },
+ {
+ blackcard_id: 9,
+whitecard_id: 10,
+score: 2
+ },
+ {
+ blackcard_id: 9,
+whitecard_id: 11,
+score: 9
+ },
+ {
+ blackcard_id: 9,
+whitecard_id: 12,
+score: 0
+ },
+ {
+ blackcard_id: 9,
+whitecard_id: 13,
+score: 8
+ },
+ {
+ blackcard_id: 9,
+whitecard_id: 14,
+score: 0
+ },
+ {
+ blackcard_id: 9,
+whitecard_id: 15,
+score: 5
+ },
+ {
+ blackcard_id: 9,
+whitecard_id: 16,
+score: 8
+ },
+ {
+ blackcard_id: 9,
+whitecard_id: 17,
+score: 9
+ },
+ {
+ blackcard_id: 10,
+whitecard_id: 1,
+score: 8
+ },
+ {
+ blackcard_id: 10,
+whitecard_id: 2,
+score: 0
+ },
+ {
+ blackcard_id: 10,
+whitecard_id: 3,
+score: 4
+ },
+ {
+ blackcard_id: 10,
+whitecard_id: 4,
+score: 8
+ },
+ {
+ blackcard_id: 10,
+whitecard_id: 5,
+score: 8
+ },
+ {
+ blackcard_id: 10,
+whitecard_id: 6,
+score: 3
+ },
+ {
+ blackcard_id: 10,
+whitecard_id: 7,
+score: 2
+ },
+ {
+ blackcard_id: 10,
+whitecard_id: 8,
+score: 7
+ },
+ {
+ blackcard_id: 10,
+whitecard_id: 9,
+score: 9
+ },
+ {
+ blackcard_id: 10,
+whitecard_id: 10,
+score: 1
+ },
+ {
+ blackcard_id: 10,
+whitecard_id: 11,
+score: 7
+ },
+ {
+ blackcard_id: 10,
+whitecard_id: 12,
+score: 9
+ },
+ {
+ blackcard_id: 10,
+whitecard_id: 13,
+score: 6
+ },
+ {
+ blackcard_id: 10,
+whitecard_id: 14,
+score: 2
+ },
+ {
+ blackcard_id: 10,
+whitecard_id: 15,
+score: 0
+ },
+ {
+ blackcard_id: 10,
+whitecard_id: 16,
+score: 9
+ },
+ {
+ blackcard_id: 10,
+whitecard_id: 17,
+score: 4
+ },
+ {
+ blackcard_id: 11,
+whitecard_id: 1,
+score: 5
+ },
+ {
+ blackcard_id: 11,
+whitecard_id: 2,
+score: 5
+ },
+ {
+ blackcard_id: 11,
+whitecard_id: 3,
+score: 7
+ },
+ {
+ blackcard_id: 11,
+whitecard_id: 4,
+score: 3
+ },
+ {
+ blackcard_id: 11,
+whitecard_id: 5,
+score: 4
+ },
+ {
+ blackcard_id: 11,
+whitecard_id: 6,
+score: 6
+ },
+ {
+ blackcard_id: 11,
+whitecard_id: 7,
+score: 6
+ },
+ {
+ blackcard_id: 11,
+whitecard_id: 8,
+score: 5
+ },
+ {
+ blackcard_id: 11,
+whitecard_id: 9,
+score: 6
+ },
+ {
+ blackcard_id: 11,
+whitecard_id: 10,
+score: 5
+ },
+ {
+ blackcard_id: 11,
+whitecard_id: 11,
+score: 3
+ },
+ {
+ blackcard_id: 11,
+whitecard_id: 12,
+score: 7
+ },
+ {
+ blackcard_id: 11,
+whitecard_id: 13,
+score: 1
+ },
+ {
+ blackcard_id: 11,
+whitecard_id: 14,
+score: 2
+ },
+ {
+ blackcard_id: 11,
+whitecard_id: 15,
+score: 0
+ },
+ {
+ blackcard_id: 11,
+whitecard_id: 16,
+score: 0
+ },
+ {
+ blackcard_id: 11,
+whitecard_id: 17,
+score: 5
+ },
+ {
+ blackcard_id: 12,
+whitecard_id: 1,
+score: 6
+ },
+ {
+ blackcard_id: 12,
+whitecard_id: 2,
+score: 1
+ },
+ {
+ blackcard_id: 12,
+whitecard_id: 3,
+score: 8
+ },
+ {
+ blackcard_id: 12,
+whitecard_id: 4,
+score: 0
+ },
+ {
+ blackcard_id: 12,
+whitecard_id: 5,
+score: 6
+ },
+ {
+ blackcard_id: 12,
+whitecard_id: 6,
+score: 8
+ },
+ {
+ blackcard_id: 12,
+whitecard_id: 7,
+score: 7
+ },
+ {
+ blackcard_id: 12,
+whitecard_id: 8,
+score: 0
+ },
+ {
+ blackcard_id: 12,
+whitecard_id: 9,
+score: 4
+ },
+ {
+ blackcard_id: 12,
+whitecard_id: 10,
+score: 6
+ },
+ {
+ blackcard_id: 12,
+whitecard_id: 11,
+score: 2
+ },
+ {
+ blackcard_id: 12,
+whitecard_id: 12,
+score: 3
+ },
+ {
+ blackcard_id: 12,
+whitecard_id: 13,
+score: 3
+ },
+ {
+ blackcard_id: 12,
+whitecard_id: 14,
+score: 6
+ },
+ {
+ blackcard_id: 12,
+whitecard_id: 15,
+score: 3
+ },
+ {
+ blackcard_id: 12,
+whitecard_id: 16,
+score: 7
+ },
+ {
+ blackcard_id: 12,
+whitecard_id: 17,
+score: 4
+ },
+ {
+ blackcard_id: 13,
+whitecard_id: 1,
+score: 8
+ },
+ {
+ blackcard_id: 13,
+whitecard_id: 2,
+score: 5
+ },
+ {
+ blackcard_id: 13,
+whitecard_id: 3,
+score: 2
+ },
+ {
+ blackcard_id: 13,
+whitecard_id: 4,
+score: 2
+ },
+ {
+ blackcard_id: 13,
+whitecard_id: 5,
+score: 6
+ },
+ {
+ blackcard_id: 13,
+whitecard_id: 6,
+score: 3
+ },
+ {
+ blackcard_id: 13,
+whitecard_id: 7,
+score: 2
+ },
+ {
+ blackcard_id: 13,
+whitecard_id: 8,
+score: 8
+ },
+ {
+ blackcard_id: 13,
+whitecard_id: 9,
+score: 3
+ },
+ {
+ blackcard_id: 13,
+whitecard_id: 10,
+score: 6
+ },
+ {
+ blackcard_id: 13,
+whitecard_id: 11,
+score: 3
+ },
+ {
+ blackcard_id: 13,
+whitecard_id: 12,
+score: 5
+ },
+ {
+ blackcard_id: 13,
+whitecard_id: 13,
+score: 7
+ },
+ {
+ blackcard_id: 13,
+whitecard_id: 14,
+score: 3
+ },
+ {
+ blackcard_id: 13,
+whitecard_id: 15,
+score: 0
+ },
+ {
+ blackcard_id: 13,
+whitecard_id: 16,
+score: 9
+ },
+ {
+ blackcard_id: 13,
+whitecard_id: 17,
+score: 1
+ },
+ {
+ blackcard_id: 14,
+whitecard_id: 1,
+score: 4
+ },
+ {
+ blackcard_id: 14,
+whitecard_id: 2,
+score: 1
+ },
+ {
+ blackcard_id: 14,
+whitecard_id: 3,
+score: 5
+ },
+ {
+ blackcard_id: 14,
+whitecard_id: 4,
+score: 1
+ },
+ {
+ blackcard_id: 14,
+whitecard_id: 5,
+score: 6
+ },
+ {
+ blackcard_id: 14,
+whitecard_id: 6,
+score: 3
+ },
+ {
+ blackcard_id: 14,
+whitecard_id: 7,
+score: 7
+ },
+ {
+ blackcard_id: 14,
+whitecard_id: 8,
+score: 4
+ },
+ {
+ blackcard_id: 14,
+whitecard_id: 9,
+score: 9
+ },
+ {
+ blackcard_id: 14,
+whitecard_id: 10,
+score: 6
+ },
+ {
+ blackcard_id: 14,
+whitecard_id: 11,
+score: 4
+ },
+ {
+ blackcard_id: 14,
+whitecard_id: 12,
+score: 4
+ },
+ {
+ blackcard_id: 14,
+whitecard_id: 13,
+score: 1
+ },
+ {
+ blackcard_id: 14,
+whitecard_id: 14,
+score: 5
+ },
+ {
+ blackcard_id: 14,
+whitecard_id: 15,
+score: 6
+ },
+ {
+ blackcard_id: 14,
+whitecard_id: 16,
+score: 2
+ },
+ {
+ blackcard_id: 14,
+whitecard_id: 17,
+score: 3
+ },
+ {
+ blackcard_id: 15,
+whitecard_id: 1,
+score: 0
+ },
+ {
+ blackcard_id: 15,
+whitecard_id: 2,
+score: 7
+ },
+ {
+ blackcard_id: 15,
+whitecard_id: 3,
+score: 7
+ },
+ {
+ blackcard_id: 15,
+whitecard_id: 4,
+score: 7
+ },
+ {
+ blackcard_id: 15,
+whitecard_id: 5,
+score: 1
+ },
+ {
+ blackcard_id: 15,
+whitecard_id: 6,
+score: 9
+ },
+ {
+ blackcard_id: 15,
+whitecard_id: 7,
+score: 4
+ },
+ {
+ blackcard_id: 15,
+whitecard_id: 8,
+score: 4
+ },
+ {
+ blackcard_id: 15,
+whitecard_id: 9,
+score: 7
+ },
+ {
+ blackcard_id: 15,
+whitecard_id: 10,
+score: 1
+ },
+ {
+ blackcard_id: 15,
+whitecard_id: 11,
+score: 9
+ },
+ {
+ blackcard_id: 15,
+whitecard_id: 12,
+score: 0
+ },
+ {
+ blackcard_id: 15,
+whitecard_id: 13,
+score: 4
+ },
+ {
+ blackcard_id: 15,
+whitecard_id: 14,
+score: 9
+ },
+ {
+ blackcard_id: 15,
+whitecard_id: 15,
+score: 6
+ },
+ {
+ blackcard_id: 15,
+whitecard_id: 16,
+score: 7
+ },
+ {
+ blackcard_id: 15,
+whitecard_id: 17,
+score: 0
+ }
+ ])
